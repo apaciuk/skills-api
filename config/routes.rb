17de9169f1272ced6deae60083577fc625f99e07
Paul Anthony McGowan <xhostcom@aol.com>
2023-02-12 22:02:15 +0000

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :train_dates
-  resources :goals
-
   namespace :api do
     namespace :v1 do
     devise_for :users, controllers: { sessions: "api/v1/sessions", registrations: "api/v1/registrations" }
-  resources :pages
-  resources :skills
+    resources :pages
+    resources :skills 
+    resources :goals
+    resources :train_dates
+    resources :users
     end 
   end
 end
