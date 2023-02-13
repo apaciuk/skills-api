1188610b12484b7a61e7c0f82aee58abd1ea552a
Paul Anthony McGowan <xhostcom@aol.com>
2023-02-13 09:28:36 +0000

    Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+require 'active_support/all'
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+    include ActionController::RequestForgeryProtection
+    # protect_from_forgery with: :exception 
+
+    private 
+
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
 end
--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,21 @@
 class GoalsController < ApplicationController
+    def new 
+    user = User.find_by(id: session[:user_id])
+    goal = goal = Goal.new(goal_params)
+    goal.user = user 
+    goal.exp = 10
+    goal.ended = false 
+        if goal.save
+            render json: UserSerializer.new(user)
+        else
+            render json: {error: "Goal not created"}
+        end
+    end 
+
+    private 
+
+    def goal_params
+        params.require(:goals).permit(:name, :timeframe, :exp, :user_id, :goal_id)
+    end 
+
 end
