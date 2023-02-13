b8f169395cbf31115dce763a49a34003b7aa6f6b
Paul Anthony McGowan <xhostcom@aol.com>
2023-02-13 18:16:32 +0000

    User model

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,4 +1,18 @@
 class GoalsController < ApplicationController
+
+    def create 
+        user = User.find_by(id: session[:user_id])
+        goal = Goal.new(goal_params)
+        goal.user = user
+        goal.exp = 10
+        goal.ended = false
+        if goal.save
+            render json: UserSerializer.new(user)
+        else
+            render json: {error: "Goal not created"}
+        end
+    end
+    
     def new 
     user = User.find_by(id: session[:user_id])
     goal = goal = Goal.new(goal_params)
@@ -11,6 +25,26 @@ class GoalsController < ApplicationController
             render json: {error: "Goal not created"}
         end
     end 
+
+    def edit
+        goal = Goal.find_by(id: params[:goal_id])
+        user = User.find_by(id: goal_params[:user_id])
+        goal.update(ended: true)
+        user.user_level += 1 
+        user.save 
+        todays_date = Time.now.strftime("%m/%d/%Y") 
+        if goal.save 
+            train = TrainDate.find_or_create_by(date: todays_date)
+            train.count +=1  
+            train.user = user 
+            train.save 
+            render json: UserSerializer.new(user)
+        else
+            render json: {error: "Goal not updated"}
+        end
+    end 
+
+
 
     private 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   has_many :goals, dependent: :destroy, class_name: 'Goal', foreign_key: 'user_id'
   has_many :skills, dependent: :destroy, class_name: 'Skill', foreign_key: 'user_id'
-  has_many :train_dates, dependent: :destroy, class_name: 'TrainDate', foreign_key: 'user_id'
+  has_many :train_dates, -> { distinct }, dependent: :destroy, class_name: 'TrainDate', foreign_key: 'user_id'
 end
